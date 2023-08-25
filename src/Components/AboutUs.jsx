53db6baf20e94973b40173df6a80faefd456bcb4
Eisha313 <eishakamran313@gmail.com>
2023-08-25 06:24:33 -0700

    i incorporated usquare solution map in about us page

--- src/Components/AboutUs.jsx
+++ src/Components/AboutUs.jsx
@@ -1,8 +1,4 @@
-// export default AboutUs=()=>{
-//     return(
-//         <Text>Usquare Solutions is the leading digital platform for all business firms across the world and an one stop automated solution for your trade and industry. No more and no less. We work with you to develop the solutions that you need and that will carry you into the digital future. Individual and tailor-made. Depending on the size and field of your organization, we have different products and services to meet your requirements. We provide the optimum and customized solutions made for your organization.</Text>
-//     )
-// }
+
 import React from "react";
 import GoogleMapReact from 'google-map-react';
 import { Text ,Title} from "@mantine/core";
