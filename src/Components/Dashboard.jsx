33dc0f279441a0518456ba2581432ae8a521adbb
Eisha313 <eishakamran313@gmail.com>
2023-08-31 23:07:38 -0700

    i did the dashboard stats

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,54 +1,89 @@
 import { Box, SimpleGrid } from "@mantine/core";
-import React from "react";
+import {React,useEffect,useState} from "react";
 import DashboardStat from "./DashboardStat";
 import BarChart from "../../utils/BarChart";
 import PieChartData from "../../utils/PieChartData";
 import { Car, MessageCircle, MessageReport, ShoppingCart, Users } from "tabler-icons-react";
+import axios from "axios";
+
 
 const Dashboard = () => {
 
-    const dashboardStats = [
-        {
-          Heading: "Total Users",
-          Count: 7,
-          icon: <Users strokeWidth={2} color="orange" size={20} />,
-        },
-        {
-          Heading: "Total Orders",
-          Count: 5,
-          icon: <ShoppingCart size={20} strokeWidth={2} color={"orange"} />,
-        },
-        {
-          Heading: "Total Vehicles",
-          Count: 7,
-          icon: <Car size={20} strokeWidth={2} color={"orange"} />,
-        },
-        {
-          Heading: "Total Payments",
-          Count: 7,
-          icon: <Users strokeWidth={2} color="orange" size={20} />,
-        },
-        {
-          Heading: "Total Complaints",
-          Count: 7,
-          icon: <MessageReport size={20} strokeWidth={2} color={"orange"} />,
-        },
-        {
-          Heading: "Total Reviews",
-          Count: 7,
-          icon: <MessageCircle size={20} strokeWidth={2} color={"orange"} />,
-        },
-        {
-          Heading: "Total Accounts",
-          Count: 7,
-          icon: <Users strokeWidth={2} color="orange" size={20} />,
-        },
-        {
-          Heading: "Total Vehicles Delivered",
-          Count: 7,
-          icon: <Car size={20} strokeWidth={2} color={"orange"} />,
-        },
-      ];
+  const [statData, setStatData] = useState(null); 
+
+    // const dashboardStats = [
+    //     {
+    //       Heading: "Total Users",
+    //       Count: 7,
+    //       icon: <Users strokeWidth={2} color="orange" size={20} />,
+    //     },
+    //     {
+    //       Heading: "Total Orders",
+    //       Count: 5,
+    //       icon: <ShoppingCart size={20} strokeWidth={2} color={"orange"} />,
+    //     },
+    //     {
+    //       Heading: "Total Vehicles",
+    //       Count: 7,
+    //       icon: <Car size={20} strokeWidth={2} color={"orange"} />,
+    //     },
+    //     {
+    //       Heading: "Total Payments",
+    //       Count: 7,
+    //       icon: <Users strokeWidth={2} color="orange" size={20} />,
+    //     },
+    //     {
+    //       Heading: "Total Complaints",
+    //       Count: 7,
+    //       icon: <MessageReport size={20} strokeWidth={2} color={"orange"} />,
+    //     },
+    //     {
+    //       Heading: "Total Reviews",
+    //       Count: 7,
+    //       icon: <MessageCircle size={20} strokeWidth={2} color={"orange"} />,
+    //     },
+    //     {
+    //       Heading: "Total Accounts",
+    //       Count: 7,
+    //       icon: <Users strokeWidth={2} color="orange" size={20} />,
+    //     },
+    //     {
+    //       Heading: "Total Vehicles Delivered",
+    //       Count: 7,
+    //       icon: <Car size={20} strokeWidth={2} color={"orange"} />,
+    //     },
+    //   ];
+    useEffect(() => {
+      const JWT="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImNyZWF0ZWRBdCI6MTY4ODM4ODQ3MDg4OCwidXBkYXRlZEF0IjoxNjkyOTY5ODcxMTEyLCJpZCI6IjY0YTJjMzc2YTIzNjQ5MDAxNDkyZjIzMSIsInN0YXR1cyI6IkFjdGl2ZSIsInVzZXJUeXBlIjoiQWRtaW4iLCJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsImZpcnN0TmFtZSI6Ik1pc3RlciIsIm1pZGRsZU5hbWUiOm51bGwsImxhc3ROYW1lIjoiQWRtaW4iLCJwaG9uZU51bWJlciI6IisxKDExMSktMTExLTExMTEiLCJzdGF0ZUxvY2F0aW9uIjoiSUwiLCJ6aXBDb2RlIjoiMjEwNzYiLCJwcm9maWxlUGljdHVyZSI6Imh0dHBzOi8vZmlyZWJhc2VzdG9yYWdlLmdvb2dsZWFwaXMuY29tL3YwL2IvY2FyZmx5cy1iMWI1Ny5hcHBzcG90LmNvbS9vL3Byb2ZpbGUtcGljdHVyZXMlMkYyMGY5Zjg5Ni1hOTZkLTQzZjUtYWE4Ni1hMmIxYmYyNDdlZjBpbWFnZXMucG5nP2FsdD1tZWRpYSZ0b2tlbj03Njk3OGU4Yi05NTg0LTQxNjYtYjc2NC02Yzc1MmRiM2ZlNTEiLCJlbWFpbFZlcmlmaWVkIjpmYWxzZSwidmVyaWZpY2F0aW9uVG9rZW4iOiIiLCJ2ZXJpZmljYXRpb25Ub2tlbkV4cGlyZXNBdCI6IiIsInN0cmlwZUNvbm5lY3RlZEFjY291bnRJZCI6ImFjY3RfMU5qMDAxREJkb1hZaFM5YSIsInJpZ2h0cyI6eyJtYW5hZ2VFbXBsb3llZXMiOnRydWUsIm1hbmFnZVZlaGljbGVzIjp0cnVlLCJtYW5hZ2VPcmRlcnMiOnRydWUsIm1hbmFnZVBheW1lbnRzIjp0cnVlLCJtYW5hZ2VBY2NvdW50cyI6dHJ1ZSwibWFuYWdlUmV2aWV3cyI6dHJ1ZSwibWFuYWdlQ29tcGxhaW50cyI6dHJ1ZSwibWFuYWdlU2V0dGluZ3MiOnRydWV9LCJkZWFsZXJzaGlwIjpudWxsfSwiaWF0IjoxNjkzNDgwNzg0LCJleHAiOjE2OTYwNzI3ODR9.xU2xbjAaf3o4H41VLmEUlC1dCeypIiVCvGYKUxFxO2Q";
+       const fetchData = async () => {
+         
+         try {
+           const response = await axios.get('https://carflys.herokuapp.com/dashboard/get-stats', {
+
+             
+             headers: {
+               Authorization: JWT, 
+             },
+           });
+           if (response.data.message === "Success") {
+            setStatData(response.data.data);
+          } else {
+            console.error('Failed to fetch data');
+          }
+   
+          //  if (response.ok) {
+          //    const data = await response.json();
+          //    setStatData(data);
+          //  } else {
+          //    console.error('Failed to fetch data');
+          //  }
+         } catch (error) {
+           console.error('Error fetching data:', error);
+         }
+       };
+   
+       fetchData();
+     }, []);
       const Data = [
         {
           id: 1,
@@ -108,13 +143,68 @@ const Dashboard = () => {
           { maxWidth: "xs", cols: 1, spacing: "md" },
         ]}
       >
-        {dashboardStats.map((item, index) => (
+        {/* {dashboardStats.map((item, index) => (
           <DashboardStat
             heading={item.Heading}
             count={item.Count}
             icon={item.icon}
           />
-        ))}
+        ))} */}
+         {/* {statData &&
+    statData.map((item, index) => (
+      <DashboardStat
+        key={index} 
+        heading={item.Heading}
+        count={item.Count}
+        icon={item.icon}
+      />
+    ))} */}
+    {statData && (
+  <>
+    <DashboardStat
+      heading="Total Users"
+      count={statData.users}
+      icon={<Users strokeWidth={2} color="orange" size={20} />}
+    />
+    <DashboardStat
+      heading="Total Orders"
+      count={statData.orders}
+      icon={<ShoppingCart size={20} strokeWidth={2} color={"orange"} />}
+    />
+    <DashboardStat
+      heading="Total Vehicles"
+      count={statData.vehicles}
+      icon={<Car size={20} strokeWidth={2} color={"orange"} />}
+    />
+    
+    <DashboardStat
+      heading="Total Payments"
+      count={statData.payments}
+      icon={<Users strokeWidth={2} color="orange" size={20} />}
+    />
+    <DashboardStat
+      heading="Total Complaints"
+      count={statData.complaints}
+      icon={<MessageReport size={20} strokeWidth={2} color={"orange"} />}
+    />
+    <DashboardStat
+      heading="Total Reviews"
+      count={statData.reviews}
+      icon={<MessageCircle size={20} strokeWidth={2} color={"orange"} />}
+    />
+    <DashboardStat
+      heading="Total Accounts"
+      count={statData.accounts}
+      icon={<Users strokeWidth={2} color="orange" size={20} />}
+    />
+    <DashboardStat
+      heading="Total Vehicles Delivered"
+      count={statData.receivedVehicles}
+      icon={<Car size={20} strokeWidth={2} color={"orange"} />}
+    />
+   
+  </>
+)}
       </SimpleGrid>
       <Box
         p="md"
--- src/Components/DashboardStat.jsx
+++ src/Components/DashboardStat.jsx
@@ -1,8 +1,34 @@
 import { Flex, Text } from '@mantine/core'
-import React from 'react'
-import { Users } from 'tabler-icons-react'
+import {React,useEffect} from 'react'
+import { Users } from 'tabler-icons-react';
+import axios from 'axios';
 
 const DashboardStat = ({ heading , count,icon}) => {
+  // useEffect(() => {
+  //  const JWT="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImNyZWF0ZWRBdCI6MTY4ODM4ODQ3MDg4OCwidXBkYXRlZEF0IjoxNjkyOTY5ODcxMTEyLCJpZCI6IjY0YTJjMzc2YTIzNjQ5MDAxNDkyZjIzMSIsInN0YXR1cyI6IkFjdGl2ZSIsInVzZXJUeXBlIjoiQWRtaW4iLCJlbWFpbCI6ImFkbWluQGdtYWlsLmNvbSIsImZpcnN0TmFtZSI6Ik1pc3RlciIsIm1pZGRsZU5hbWUiOm51bGwsImxhc3ROYW1lIjoiQWRtaW4iLCJwaG9uZU51bWJlciI6IisxKDExMSktMTExLTExMTEiLCJzdGF0ZUxvY2F0aW9uIjoiSUwiLCJ6aXBDb2RlIjoiMjEwNzYiLCJwcm9maWxlUGljdHVyZSI6Imh0dHBzOi8vZmlyZWJhc2VzdG9yYWdlLmdvb2dsZWFwaXMuY29tL3YwL2IvY2FyZmx5cy1iMWI1Ny5hcHBzcG90LmNvbS9vL3Byb2ZpbGUtcGljdHVyZXMlMkYyMGY5Zjg5Ni1hOTZkLTQzZjUtYWE4Ni1hMmIxYmYyNDdlZjBpbWFnZXMucG5nP2FsdD1tZWRpYSZ0b2tlbj03Njk3OGU4Yi05NTg0LTQxNjYtYjc2NC02Yzc1MmRiM2ZlNTEiLCJlbWFpbFZlcmlmaWVkIjpmYWxzZSwidmVyaWZpY2F0aW9uVG9rZW4iOiIiLCJ2ZXJpZmljYXRpb25Ub2tlbkV4cGlyZXNBdCI6IiIsInN0cmlwZUNvbm5lY3RlZEFjY291bnRJZCI6ImFjY3RfMU5qMDAxREJkb1hZaFM5YSIsInJpZ2h0cyI6eyJtYW5hZ2VFbXBsb3llZXMiOnRydWUsIm1hbmFnZVZlaGljbGVzIjp0cnVlLCJtYW5hZ2VPcmRlcnMiOnRydWUsIm1hbmFnZVBheW1lbnRzIjp0cnVlLCJtYW5hZ2VBY2NvdW50cyI6dHJ1ZSwibWFuYWdlUmV2aWV3cyI6dHJ1ZSwibWFuYWdlQ29tcGxhaW50cyI6dHJ1ZSwibWFuYWdlU2V0dGluZ3MiOnRydWV9LCJkZWFsZXJzaGlwIjpudWxsfSwiaWF0IjoxNjkzNDgwNzg0LCJleHAiOjE2OTYwNzI3ODR9.xU2xbjAaf3o4H41VLmEUlC1dCeypIiVCvGYKUxFxO2Q";
+  //   const fetchData = async () => {
+      
+  //     try {
+  //       const response = await axios.get('https://carflys.herokuapp.com/dashboard/get-stats', {
+          
+  //         headers: {
+  //           Authorization: JWT, 
+  //         },
+  //       });
+
+  //       if (response.ok) {
+  //         const data = await response.json();
+  //         setStatData(data);
+  //       } else {
+  //         console.error('Failed to fetch data');
+  //       }
+  //     } catch (error) {
+  //       console.error('Error fetching data:', error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
 
   
 
